d92e979d816943d8c45e62e6a95f46ede7296a93
diodonfrost <diodon.frost@diodonfrost.me>
2026-01-19 21:16:43 +0000

    tests(terraform): add scale set scheduler example
    Add example configuration for Linux Virtual Machine
    Scale Set stop/start scheduling with networking infrastructure,
    SSH key generation, and tag-based targeting to demonstrate cost
    optimization by deallocating scale sets outside business hours.

---
+++ examples/scale_set_scheduler/main.tf
@@ -0,0 +1,141 @@
+resource "random_pet" "suffix" {}
+
+resource "random_id" "suffix" {
+  byte_length = 6
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "test-${random_pet.suffix.id}"
+  location = "swedencentral"
+}
+
+resource "azurerm_log_analytics_workspace" "test" {
+  name                = "test-${random_pet.suffix.id}"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+
+resource "azurerm_linux_virtual_machine_scale_set" "to_stop" {
+  count = 2
+
+  name                = "test-to-stop-${count.index}-${random_pet.suffix.id}"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  sku                 = "Standard_B1s"
+  instances           = 2
+  admin_username      = "adminuser"
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = tls_private_key.test.public_key_openssh
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  network_interface {
+    name    = "test-${count.index}"
+    primary = true
+
+    ip_configuration {
+      name      = "internal"
+      primary   = true
+      subnet_id = azurerm_subnet.test.id
+    }
+  }
+
+  tags = {
+    tostop = "true"
+  }
+}
+
+resource "azurerm_linux_virtual_machine_scale_set" "do_not_stop" {
+  count = 2
+
+  name                = "test-do-not-stop-${count.index}-${random_pet.suffix.id}"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  sku                 = "Standard_B1s"
+  instances           = 2
+  admin_username      = "adminuser"
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = tls_private_key.test.public_key_openssh
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+
+  os_disk {
+    storage_account_type = "Standard_LRS"
+    caching              = "ReadWrite"
+  }
+
+  network_interface {
+    name    = "test-${count.index}"
+    primary = true
+
+    ip_configuration {
+      name      = "internal"
+      primary   = true
+      subnet_id = azurerm_subnet.test.id
+    }
+  }
+
+  tags = {
+    tostop = "false"
+  }
+}
+
+resource "tls_private_key" "test" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+
+module "stop_scale_sets" {
+  source = "../../"
+
+  resource_group_name          = azurerm_resource_group.test.name
+  location                     = azurerm_resource_group.test.location
+  container_app_job_name       = "caj-to-stop-${random_pet.suffix.id}"
+  scheduler_action             = "stop"
+  scheduler_crontab_expression = "0 22 * * *"
+  scale_set_schedule           = "true"
+  log_analytics_workspace_id   = azurerm_log_analytics_workspace.test.id
+  scheduler_tag = {
+    tostop = "true"
+  }
+}
+
+module "start_scale_sets" {
+  source = "../../"
+
+  resource_group_name          = azurerm_resource_group.test.name
+  location                     = azurerm_resource_group.test.location
+  container_app_job_name       = "caj-to-start-${random_pet.suffix.id}"
+  scheduler_action             = "start"
+  scheduler_crontab_expression = "0 7 * * *"
+  scale_set_schedule           = "true"
+  log_analytics_workspace_id   = azurerm_log_analytics_workspace.test.id
+  scheduler_tag = {
+    tostop = "true"
+  }
+}

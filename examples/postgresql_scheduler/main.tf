3d42ac3d5c18d10993987251fb1908f199f5908a
diodonfrost <diodon.frost@diodonfrost.me>
2026-01-19 21:10:41 +0000

    tests(terraform): add PostgreSQL scheduler example
    Add example configuration for PostgreSQL Flexible Server
    stop/start scheduling with test infrastructure including
    servers tagged for stopping and servers that should remain running.

---
+++ examples/postgresql_scheduler/main.tf
@@ -0,0 +1,89 @@
+resource "random_pet" "suffix" {}
+
+resource "random_id" "suffix" {
+  byte_length = 6
+}
+
+resource "azurerm_resource_group" "test" {
+  name     = "test-${random_pet.suffix.id}"
+  location = "swedencentral"
+}
+
+resource "azurerm_log_analytics_workspace" "test" {
+  name                = "test-${random_pet.suffix.id}"
+  location            = azurerm_resource_group.test.location
+  resource_group_name = azurerm_resource_group.test.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
+
+resource "azurerm_postgresql_flexible_server" "to_stop" {
+  count = 2
+
+  name                   = "test-to-stop-${count.index}-${random_pet.suffix.id}"
+  resource_group_name    = azurerm_resource_group.test.name
+  location               = azurerm_resource_group.test.location
+  version                = "15"
+  administrator_login    = "psqladmin"
+  administrator_password = "H@Sh1CoR3!"
+  zone                   = "1"
+
+  storage_mb = 32768
+
+  sku_name = "B_Standard_B1ms"
+
+  tags = {
+    tostop = "true"
+  }
+}
+
+resource "azurerm_postgresql_flexible_server" "do_not_stop" {
+  count = 2
+
+  name                   = "test-do-not-stop-${count.index}-${random_pet.suffix.id}"
+  resource_group_name    = azurerm_resource_group.test.name
+  location               = azurerm_resource_group.test.location
+  version                = "15"
+  administrator_login    = "psqladmin"
+  administrator_password = "H@Sh1CoR3!"
+  zone                   = "1"
+
+  storage_mb = 32768
+
+  sku_name = "B_Standard_B1ms"
+
+  tags = {
+    tostop = "false"
+  }
+}
+
+
+module "stop_postgresql" {
+  source = "../../"
+
+  resource_group_name          = azurerm_resource_group.test.name
+  location                     = azurerm_resource_group.test.location
+  container_app_job_name       = "caj-to-stop-${random_pet.suffix.id}"
+  scheduler_action             = "stop"
+  scheduler_crontab_expression = "0 22 * * *"
+  postgresql_schedule          = "true"
+  log_analytics_workspace_id   = azurerm_log_analytics_workspace.test.id
+  scheduler_tag = {
+    tostop = "true"
+  }
+}
+
+module "start_postgresql" {
+  source = "../../"
+
+  resource_group_name          = azurerm_resource_group.test.name
+  location                     = azurerm_resource_group.test.location
+  container_app_job_name       = "caj-to-start-${random_pet.suffix.id}"
+  scheduler_action             = "start"
+  scheduler_crontab_expression = "0 7 * * *"
+  postgresql_schedule          = "true"
+  log_analytics_workspace_id   = azurerm_log_analytics_workspace.test.id
+  scheduler_tag = {
+    tostop = "true"
+  }
+}
